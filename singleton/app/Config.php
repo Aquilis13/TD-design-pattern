5ff71d673644de69de4e7d9128b69ccaf9a44274
Aquilis13 <chanot.flora@gmail.com>
2024-03-11 14:57:01 +0100

    Exercice singleton

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,40 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+class Config {
+    private static $_instance;
+    private $settings;
+
+    /**
+     * Récupère les paramètres du fichier config.php dans le répertoire config
+     * 
+     */
+    private function __construct(){
+        $this->settings = include('../config/config.php');
+    }
+
+    /**
+     * Retourne l'instance de la classe Config. 
+     * Si elle n'existe pas on la crée.
+     * 
+     */
+    public static function getInstance(){
+        if(is_null(self::$_instance)){
+            self::$_instance = new Config();
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Retourne une clé passer en paramètre dans le fichier config
+     * 
+     */
+    public function get($key){
+        if($key == "debug" || $key == "apiKey"){
+            return $this->settings[$key];
+        }else{
+            return $this->settings['db'][$key];
+        }        
+    }
+} 
